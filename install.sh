f5a89d20b25305e119ed34f3b5ad38bc734389dd
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-04-06 22:55:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -630,6 +630,11 @@ wget -q -O /etc/systemd/system/ws.service "${REPO}source/ws.service"
 chmod +x /etc/systemd/system/ws.service
 chmod +x /usr/bin/ws
 chmod 644 /usr/bin/tun.conf
+systemctl disable ws
+systemctl stop ws
+systemctl enable ws
+systemctl start ws
+systemctl restart ws
 wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
 wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
 wget -O /usr/sbin/ftvpn "${REPO}source/ftvpn" >/dev/null 2>&1

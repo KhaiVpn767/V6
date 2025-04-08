b8c01ec6cc61a57f586c22e61dac541860c8b0d2
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-04-08 17:24:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -600,9 +600,9 @@ wget -q -O /etc/issue.net "https://raw.githubusercontent.com/KhaiVpn767/V6/main/
 function ins_epro(){
 clear
 print_install "Install ePro WebSocket Proxy"
-    wget -O /usr/bin/ws "https://raw.githubusercontent.com/KhaiVpn767/V6/main/files/ws" >/dev/null 2>&1
-    wget -O /usr/bin/tun.conf "https://raw.githubusercontent.com/KhaiVpn767/V6/main/config/tun.conf" >/dev/null 2>&1
-    wget -O /etc/systemd/system/ws.service "https://raw.githubusercontent.com/KhaiVpn767/V6/main/files/ws.service" >/dev/null 2>&1
+    wget -O /usr/bin/ws "https://raw.githubusercontent.com/KhaiVpn767/V6/main/source/ws" >/dev/null 2>&1
+    wget -O /usr/bin/tun.conf "https://raw.githubusercontent.com/KhaiVpn767/V6/main/source/tun.conf" >/dev/null 2>&1
+    wget -O /etc/systemd/system/ws.service "https://raw.githubusercontent.com/KhaiVpn767/V6/main/source/ws.service" >/dev/null 2>&1
     chmod +x /etc/systemd/system/ws.service
     chmod +x /usr/bin/ws
     chmod 644 /usr/bin/tun.conf
@@ -613,7 +613,7 @@ systemctl start ws
 systemctl restart ws
 wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
 wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
-wget -O /usr/sbin/ftvpn "https://raw.githubusercontent.com/KhaiVpn767/V6/main/files/ftvpn" >/dev/null 2>&1
+wget -O /usr/sbin/ftvpn "https://raw.githubusercontent.com/KhaiVpn767/V6/main/source/ftvpn" >/dev/null 2>&1
 chmod +x /usr/sbin/ftvpn
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
 iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
